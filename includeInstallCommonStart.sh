3ab15059af63fc5d8b888cc2b4d4e5ed32e5aedf
Oliver Kopp <kopp.dev@gmail.com>
2017-03-18 09:26:01 +0100

    Decrease sleep time and put nohup.logs near to the producer

--- includeInstallCommonStart.sh
+++ includeInstallCommonStart.sh
@@ -5,15 +5,15 @@ cd ~
 export JAVA_HOME="$(readlink -f /usr/bin/java | sed "s:bin/java::")";
 
 echo "Starting WSO2 BPS...";
-nohup ./wso2bps/bin/wso2server.sh >> ~/OpenTOSCA/wso2bps-nohup.log 2>&1 &
-sleep 30
+nohup ./wso2bps/bin/wso2server.sh >> ~/wso2bps/nohup.log 2>&1 &
+sleep 3
 
 echo "Starting OpenTOSCA container..."
 cd OpenTOSCA
-nohup ./OpenTOSCA >>~/OpenTOSCA/opentosca-nohup.log 2>&1 &
-sleep 30
+nohup ./OpenTOSCA >>~/OpenTOSCA/nohup.log 2>&1 &
+sleep 3
 
 echo "Startup will be finished in background...";
-echo " + Run 'tail -f ~/OpenTOSCA/*nohup.log' to see what's happening";
+echo " + Run 'tail -f ~/OpenTOSCA/nohup.log ~/wso2bps/nohup.log' to see what's happening";
 echo " + Wait a minute";
 echo " + Open 'http://<HOST>:8080' in your browser to access the OpenTOSCA ecosystem";
