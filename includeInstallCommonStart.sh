4c0e940077d03e9c24d10944be4a0bdff6fcd052
Oliver Kopp <kopp.dev@gmail.com>
2017-03-18 04:03:10 +0100

    Use more common syntax

--- includeInstallCommonStart.sh
+++ includeInstallCommonStart.sh
@@ -15,6 +15,6 @@ nohup ./OpenTOSCA >>~/OpenTOSCA/opentosca-nohup.log 2>&1 &
 sleep 30
 
 echo "Startup will be finished in background...";
-echo " + Run 'tail ~/OpenTOSCA/*nohup.log -f' to see what's happening";
+echo " + Run 'tail -f ~/OpenTOSCA/*nohup.log' to see what's happening";
 echo " + Wait a minute";
 echo " + Open 'http://<HOST>:8080' in your browser to access the OpenTOSCA ecosystem";
