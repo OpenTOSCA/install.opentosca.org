e6376e602a8d9b7be963f0920fe64f4b0a697895
Oliver Kopp <kopp.dev@gmail.com>
2017-04-23 23:49:58 +0200

    echo -> printf

--- includeInstallCommon.sh
+++ includeInstallCommon.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
 printf "Common install routines...\n"
-echo "DEBUG: x${TAG}x${CONTAINER_VERSION}x${UI_VERSION}x${WINERY_VERSION}x"
+printf "DEBUG: x${TAG}x${CONTAINER_VERSION}x${UI_VERSION}x${WINERY_VERSION}x\n"
 
 if [ -z "${TAG}${CONTAINER_VERSION}${UI_VERSION}${WINERY_VERSION}" ]; then
-  echo "A tag or specific versions have to be given"
+  printf "A tag or specific versions have to be given\n"
   exit
 fi
 
 if [ -z "$CONTAINER_VERSION" ]; then
   #in case no specific version are set, we take the tag as version for all installed components
-  echo "CONTAINER_VERISON not set. Falling back to tag."
+  printf "CONTAINER_VERISON not set. Falling back to tag.\n"
   export CONTAINER_VERSION=$TAG
   export UI_VERSION=$TAG
   export WINERY_VERSION=$TAG
 fi
 
-echo "Using container version $CONTAINER_VERSION"
-echo "Using ui version $UI_VERSION"
-echo "Using winery version $WINERY_VERSION"
+printf "Using container version $CONTAINER_VERSION"
+printf "Using ui version $UI_VERSION"
+printf "Using winery version $WINERY_VERSION"
 
 BINPATH="https://github.com/OpenTOSCA/install.opentosca.org/releases/download/$CONTAINER_VERSION"
 BUILDPATH="http://builds.opentosca.org/"
@@ -26,24 +26,24 @@ THIRDPARTYPATH="http://files.opentosca.org/third-party/$CONTAINER_VERSION"
 
 printf "\n\n### AUTOMATICALLY INSTALLING OpenTOSCA\n"
 
-echo "\n\n### Update Package List\n"
+printf "\n\n### Update Package List\n"
 sudo apt-get -y update
 
-echo "\n\n### Include security fixes\n"
+printf "\n\n### Include security fixes\n"
 sudo apt-get -y upgrade
 
-echo "\n\n### Install Tomcat 8\n"
+printf "\n\n### Install Tomcat 8\n"
 sudo apt-get -y install tomcat8 tomcat8-admin zip unzip
 sudo service tomcat8 stop
 
-echo "\n\n### Set CATALINA_OPTS\n"
+printf "\n\n### Set CATALINA_OPTS\n"
 sudo sh -c "echo 'CATALINA_OPTS=\"-Xmx1024m\"' >> /etc/default/tomcat8"
 
-echo "\n\n### Set JAVA_HOME"
+printf "\n\n### Set JAVA_HOME"
 sudo sh -c "echo 'JAVA_HOME=\"'$(readlink -f /usr/bin/java | sed "s:bin/java::")'\"' >> /etc/environment";
 export JAVA_HOME="$(readlink -f /usr/bin/java | sed "s:bin/java::")";
 
-echo "\n\n### Tomcat User Settings\n"
+printf "\n\n### Tomcat User Settings\n"
 cd ~
 wget -N $THIRDPARTYPATH/tomcat-users.xml || (echo "not found"; exit 404)
 wget -N $THIRDPARTYPATH/server.xml || (echo "not found"; exit 404)
@@ -107,35 +107,35 @@ sudo mv ./opentosca.war /var/lib/tomcat8/webapps/
 #wget -N $BINPATH/vinothek.war;
 #sudo mv ./vinothek.war /var/lib/tomcat7/webapps/vinothek.war;
 
-echo "\n\n### Install Winery\n"
+printf "\n\n### Install Winery\n"
 wget -N $BUILDPATH/winery/$WINERY_VERSION/winery.war || (echo "not found"; exit 404)
 wget -N $BUILDPATH/winery/$WINERY_VERSION/winery-topologymodeler.war || (echo "not found"; exit 404)
 sudo mv ./winery.war /var/lib/tomcat8/webapps
 sudo mv ./winery-topologymodeler.war /var/lib/tomcat8/webapps
 
-echo "\n\n### Import Winery Repository (into home)\n"
+printf "\n\n### Import Winery Repository (into home)\n"
 sudo mkdir ~tomcat8/winery-repository;
 wget -N $THIRDPARTYPATH/winery-repository.zip || (echo "not found"; exit 404)
 sudo unzip -qo winery-repository.zip -d ~tomcat8/winery-repository
 sudo chown -R tomcat8:tomcat8 ~tomcat8/winery-repository
 
-echo "\n\n### Start Tomcat\n"
+printf "\n\n### Start Tomcat\n"
 sudo service tomcat8 start
 
-echo "\n\n### Install WSO2 BPS\n"
+printf "\n\n### Install WSO2 BPS\n"
 cd ~
 wget -N $THIRDPARTYPATH/wso2bps-2.1.2-java8.zip || (echo "not found"; exit 404)
 unzip -qo wso2bps-2.1.2-java8.zip
 mv wso2bps-2.1.2/ wso2bps/
 chmod +x wso2bps/bin/wso2server.sh
 
-echo "\n\n### REST Extension\n"
+printf "\n\n### REST Extension\n"
 cd ~
 wget -N $THIRDPARTYPATH/bpel4restlight1.1.1.jar || (echo "not found"; exit 404)
 rm  wso2bps/repository/components/lib/bpel4*
 mv  bpel4restlight1.1.1.jar wso2bps/repository/components/lib/
 
-echo "\n\n### Configure REST Extension\n"
+printf "\n\n### Configure REST Extension\n"
 cd ~
 wget -N $THIRDPARTYPATH/bps.xml || (echo "not found"; exit 404)
 mv bps.xml wso2bps/repository/conf/bps.xml
@@ -157,7 +157,7 @@ sudo sed -ie "s/ExecStart=\/usr\/bin\/dockerd -H fd:\/\//ExecStart=\/usr\/bin\/d
 sudo systemctl daemon-reload
 sudo service docker start
 
-echo "\n\n### Install OpenTOSCA\n"
+printf "\n\n### Install OpenTOSCA\n"
 cd ~
 wget -N $BUILDPATH/container/$CONTAINER_VERSION/org.opentosca.container.product-linux.gtk.x86_64.zip || (echo "not found"; exit 404)
 mkdir OpenTOSCA
